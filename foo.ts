088cc88762b36e76cafa518ed31d2a2ddf3613f2
Cole Bemis <colebemis@github.com>
2024-03-19 12:01:52 -0700

    Add a foo.ts file

---
+++ foo.ts
@@ -0,0 +1,6 @@
+// Write TypeScript code that exports a foo function
+export function foo(arg: string): string {
+  // The foo function should take a string argument and return a string
+  // The foo function should concatenate the argument with "foo" and return the result
+  return arg + 'foo'
+}
